ebeb88f330dab1eba260d81fb5a01be6908dced3
Rafael Trindade <rafaellttr@gmail.com>
2024-11-26 18:13:21 -0300

    Final do módulo de Tipos e variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+        System.out.println(salarioMinimo);
+
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        System.out.println(numeroCurto2);
+
+        int numero = 1;
+        numero = 2;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14; //Para que não haja alteração, é necessário escrever em caixa alta e utilizar o final
+
+        System.out.println(VALOR_DE_PI);
     }
 }
